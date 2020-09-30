f7afc5ea9ea68115faa3c1fbed3b3816b388baca
James Butler <71946858+JamesButler121314@users.noreply.github.com>
2020-09-29 22:28:20 -0500

    Update Week6.py
    Getting closer. Still stumped on correct way to append.

--- Week6.py
+++ Week6.py
@@ -1,5 +1,5 @@
 class Vehicle:
-	"""A program to keep record of vehicles."""
+	"""A program to keep a record of your vehicles."""
 def __init__(self, make, model, color, fueltype):
 	"""Initializing Attributes."""
     self.make=make
@@ -50,48 +50,72 @@ def __init__(self, make, model, color, fueltype):
     def getBedLength(self):
        return getBedLength(self)    
 
-def selectOptions():
-    options_common = ['powerwindows', 'powerlocks', 'remotestart', 'backupcamera', 'bluetooth', 'cruisecontrol']
+
+def printGarageMenu():
+	print("[1] Please add your car.")
+	print("[2] Please add your pickup.")
+	print("[3] Would you like to see your garage?")
+	print("[4] Quit")
+
+menu()
+option = int(input("Enter your option: "))
+
+while option != 4:
+    if option == 1:
+        get.CarInput:
+    elif option == 2:
+        get.PickupInput:
+    elif option == 3:
+        
+else:
+    print("Invalid option.")
+print()
+menu()
+
+option=int(input("Enter your option: "))
+
+print("Thank you for using our Garage Program.")
+
+def selectOptions()
     options_chosen = []
-
-
-def printMenu():
-
-    print("Welcome to your virtual garage.")
-    print("[1] Add a car.")
-    print("[2] Add a truck.")
-    print("[3] See your garage.")
-    print("[4] Exit")
-
-
-print(int("Please choose an option ."))
-
-
-
+    options_common = ['powerwindows', 'powerlocks', 'remotestart', 'backupcamera', 'bluetooth', 'cruisecontrol','heatedseats', 'reardefrost']
+ 
+While (True):
+    for option in options_common:
+	print(f"option}")
+    option_chosen = input("Please choose an option to add to your vehicle.")
+    if (option_chosen.upper() =="Quit"):
+	break
+    else:
+        options_chosen.append(option_chosen)
+	
+	
 def getCarInput():
   carDetails = {}
-  carDetails["make"] = input("What is the make? ")
-  carDetails["model"] = input("What is the model? ")
-  carDetails["color"] = input("What is the color? ")
-  carDetails["fuelType"] = input("What is the fuel type? ")
+  carDetails["make"] = input("What is the make of your car? ")
+  carDetails["model"] = input("What is the model of your car? ")
+  carDetails["color"] = input("What is the color of your car? ")
+  carDetails["fuelType"] = input("What is the fuel type of your car? ")
   carDetails["options"] = selectOptions()
   carDetails["engineSize"] = input("What is the engine size? ")
-  carDetails["numDoors"] = input("How many doors? ")
+  carDetails["numDoors"] = input("How many doors does your car have? ")
 
 
 
-  def getTruckInput():
-  truckDetails = {}
-  truckDetails["make"] = input("What is the make? ")
-  truckDetails["model"] = input("What is the model? ")
-  truckDetails["color"] = input("What is the color? ")
-  truckDetails["fuelType"] = input("What is the fuel type? ")
-  truckDetails["options"] = selectOptions()
-  truckDetails["cabStyle"] = input("What is the cab style? ")
-  truckDetails["bedLength"] = input("What is the bed length? ")
+  def getPickupInput():
+  pickupDetails = {}
+  pickupDetails["make"] = input("What is the make of your pickup? ")
+  pickupDetails["model"] = input("What is the model of your pickup? ")
+  pickupDetails["color"] = input("What is the color of your pickup? ")
+  pickupDetails["fuelType"] = input("What is the fuel type or your pickup? ")
+  pickupDetails["options"] = selectOptions()
+  pickupDetails["cabStyle"] = input("What is your cab style? ")
+  pickupDetails["bedLength"] = input("What is your bed length? ")
 
   def virtualGarage(garage):
-    print('virtualGarage')
-
+    for vehicle in garage:
+	print(f{vehicle.getMake()}, {vehicle.getModel()}, {vehicle.getColor(), {vehicle.getFueltype()}')
+	vehicle.printOptions()						   
+        vehicle.display_info()
 
     
